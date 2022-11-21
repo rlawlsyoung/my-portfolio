d87e956b79693a526dee4a4ef19825e12e32a9b3
김진영 <jinyoung01099@gmail.com>
2022-11-21 21:49:14 +0900

    [Fix] Home 화면 텍스트 애니메이션 추가

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,3 +1,5 @@
+import { ReactTypical } from "@deadcoder0904/react-typical";
+import Typewriter from "typewriter-effect";
 import AboutMe from "./aboutMe/AboutMe";
 import Skills from "./skills/Skills";
 import bg from "../../assets/bg.jpg";
@@ -8,9 +10,18 @@ const Home = () => {
   return (
     <StyledHome>
       <div className="top flex-center">
-        <p className="text colorBg">안녕하세요!</p>
-        <p className="text colorBg">프론트엔드 개발자, 김진영입니다.</p>
-        <p className="small-text colorBg">
+        <Typewriter
+          onInit={(typewriter) => {
+            typewriter
+              .start()
+              .typeString("<p class='text color-bg'>안녕하세요!</p>")
+              .pauseFor(10)
+              .typeString(
+                "<p class='text color-bg'>프론트엔드 개발자, 김진영입니다.</p>"
+              );
+          }}
+        />
+        <p className="small-text color-bg flex-center">
           꾸준히 개발 지식을 쌓는 것을 좋아하고 내가 만든 서비스에 애정을 쏟는
           개발자입니다.
         </p>
@@ -22,7 +33,15 @@ const Home = () => {
 };
 
 const StyledHome = styled.div`
-  .colorBg {
+  .Typewriter__wrapper {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+  .Typewriter__cursor {
+    display: none;
+  }
+  .color-bg {
     background-color: rgba(0, 0, 0, 0.6);
   }
 
@@ -51,6 +70,19 @@ const StyledHome = styled.div`
       margin: 20px 0;
       font-size: 26px;
       font-weight: 700;
+      opacity: 0;
+      animation: fadeIn 3s;
+      animation-delay: 4.5s;
+      animation-fill-mode: forwards;
+
+      @keyframes fadeIn {
+        from {
+          opacity: 0;
+        }
+        to {
+          opacity: 1;
+        }
+      }
     }
   }
 
