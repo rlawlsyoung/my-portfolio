9d7587aba10603f83e9af3b0fb79a78c24a18fda
김진영 <jinyoung01099@gmail.com>
2022-11-29 23:41:23 +0900

    [Fix] 메인 화면 타이핑 애니메이션 수정

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,4 +1,5 @@
-import Typewriter from "typewriter-effect";
+import { useState } from "react";
+import { Typewriter } from "react-simple-typewriter";
 import AboutMe from "./aboutMe/AboutMe";
 import Skills from "./skills/Skills";
 import Portfolio from "./portfolio/Portfolio";
@@ -10,19 +11,22 @@ const Home = () => {
   return (
     <StyledHome id="메인">
       <div className="top flex-center">
-        <Typewriter
-          onInit={(typewriter) => {
-            typewriter
-              .start()
-              .typeString("<p class='text color-bg'>안녕하세요!</p>")
-              .pauseFor(10)
-              .typeString(
-                "<p class='text color-bg'>프론트엔드 개발자, 김진영입니다.</p>"
-              );
-          }}
-        />
+        <p className="text color-bg">안녕하세요!</p>
+        <p className="text color-bg">프론트엔드 개발자, 김진영입니다.</p>
+
         <p className="small-text color-bg">
-          꾸준히 개발 지식을 쌓는 것을 좋아하고 내 서비스에 애정을 쏟는
+          <Typewriter
+            words={[
+              "꾸준히 개발 지식을 쌓는 것을 좋아하는",
+              "내가 만든 서비스에 애정을 쏟는",
+              "새로운 기술을 받아들이는 것에 대한 두려움이 없는",
+            ]}
+            loop={true}
+            cursor
+            typeSpeed={70}
+            deleteSpeed={50}
+            delaySpeed={3500}
+          />
           개발자입니다.
         </p>
       </div>
@@ -34,14 +38,11 @@ const Home = () => {
 };
 
 const StyledHome = styled.div`
-  .Typewriter__wrapper {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
+  .styles-module_blinkingCursor__yugAC {
+    margin-left: -5px;
+    margin-right: 5px;
   }
-  .Typewriter__cursor {
-    display: none;
-  }
+
   .color-bg {
     background-color: rgba(0, 0, 0, 0.6);
   }
@@ -71,19 +72,6 @@ const StyledHome = styled.div`
       margin: 20px 0;
       font-size: 26px;
       font-weight: 700;
-      opacity: 0;
-      animation: fadeIn 3s;
-      animation-delay: 4.5s;
-      animation-fill-mode: forwards;
-
-      @keyframes fadeIn {
-        from {
-          opacity: 0;
-        }
-        to {
-          opacity: 1;
-        }
-      }
     }
   }
 
