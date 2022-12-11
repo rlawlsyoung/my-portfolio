3c4ca10276a663fb9851cd1a99eb40f4425c411d
김진영 <jinyoung01099@gmail.com>
2022-12-11 19:17:46 +0900

    [Fix] ProjectData 내림차순 정렬

--- src/sections/projects/ProjectData.tsx
+++ src/sections/projects/ProjectData.tsx
@@ -45,54 +45,21 @@ export const projectData: projectDataType[] = [
   },
   {
     mainImg:
-      "https://velog.velcdn.com/images/jinyoung985/post/832d2f12-819f-4282-8da1-f79f1a26ca72/image.gif",
-    isMobile: false,
-    title: "ROECY’ CHOCOLATE",
-    tags: ["전체", "팀 프로젝트"],
-    subTitle: "팀 프로젝트 (6인)",
-    mainTechs: ["JavaScript", "React.js", "sass"],
-    introduction: (
-      <>
-        ROYCE’ Chocolate 공식 홈페이지를 모티브로 한 프로젝트입니다. <br />
-        주요 기능으로는 로그인 및 회원가입, 상품 장바구니에 담기, 구매 등이
-        있습니다.
-      </>
-    ),
-    url: "https://velog.io/@jinyoung985/Portfolio-ROECY-Chocolate",
-  },
-  {
-    mainImg:
-      "https://velog.velcdn.com/images/jinyoung985/post/c9c41a3c-d588-4093-aada-a54bb42945d9/image.gif",
-    isMobile: false,
-    title: "FLOrida",
-    tags: ["전체", "팀 프로젝트"],
-    subTitle: "팀 프로젝트 (6인)",
-    mainTechs: ["JavaScript", "React.js", "styled-components"],
-    introduction: (
-      <>
-        FLO 공식 홈페이지를 모티브로 한 프로젝트입니다.
-        <br />
-        주요 기능으로는 로그인 및 회원가입, 음악 재생, 보관함에 음악 저장,
-        이용권 구매 등이 있습니다.
-      </>
-    ),
-    url: "https://velog.io/@jinyoung985/Portfolio-FLOrida-music",
-  },
-  {
-    mainImg:
-      "https://velog.velcdn.com/images/jinyoung985/post/a977ed31-c05e-41ec-a101-6720d0ac7be0/image.gif",
+      "https://velog.velcdn.com/images/jinyoung985/post/3b468dca-33de-4276-b284-d9afc34d5d0d/image.gif",
     isMobile: true,
-    title: "플레이키보드 웹 스토어",
+    title: "오디오 녹음 프로그램",
     tags: ["전체", "팀 프로젝트", "인턴십"],
-    subTitle: "인턴십 팀 프로젝트 (5인)",
-    mainTechs: ["JavaScript", "React.js", "styled-components"],
+    subTitle: "인턴십 팀 프로젝트 (3인)",
+    mainTechs: ["JavaScript", "React.js", "styled-components", "Firebase"],
     introduction: (
       <>
-        (주)비트바이트와 함께한 인턴십 팀 과제입니다.
-        <br /> 주요 기능은 카테고리 별 상품 분류, 가상 키보드 체험입니다.
+        (주)하이와 함께한 인턴십 팀 과제입니다.
+        <br />
+        주요 기능으로는 녹음, 녹음 파일 Firebase에 저장, Firebase에서 불러오기
+        등이 있습니다.
       </>
     ),
-    url: "https://velog.io/@jinyoung985/Portfolio-플레이키보드-웹-스토어-만들기",
+    url: "https://velog.io/@jinyoung985/Portfolio-오디오-녹음-재생-프로그램-만들기",
   },
   {
     mainImg:
@@ -114,20 +81,53 @@ export const projectData: projectDataType[] = [
   },
   {
     mainImg:
-      "https://velog.velcdn.com/images/jinyoung985/post/3b468dca-33de-4276-b284-d9afc34d5d0d/image.gif",
+      "https://velog.velcdn.com/images/jinyoung985/post/a977ed31-c05e-41ec-a101-6720d0ac7be0/image.gif",
     isMobile: true,
-    title: "오디오 녹음 프로그램",
+    title: "플레이키보드 웹 스토어",
     tags: ["전체", "팀 프로젝트", "인턴십"],
-    subTitle: "인턴십 팀 프로젝트 (3인)",
-    mainTechs: ["JavaScript", "React.js", "styled-components", "Firebase"],
+    subTitle: "인턴십 팀 프로젝트 (5인)",
+    mainTechs: ["JavaScript", "React.js", "styled-components"],
     introduction: (
       <>
-        (주)하이와 함께한 인턴십 팀 과제입니다.
-        <br />
-        주요 기능으로는 녹음, 녹음 파일 Firebase에 저장, Firebase에서 불러오기
-        등이 있습니다.
+        (주)비트바이트와 함께한 인턴십 팀 과제입니다.
+        <br /> 주요 기능은 카테고리 별 상품 분류, 가상 키보드 체험입니다.
       </>
     ),
-    url: "https://velog.io/@jinyoung985/Portfolio-오디오-녹음-재생-프로그램-만들기",
+    url: "https://velog.io/@jinyoung985/Portfolio-플레이키보드-웹-스토어-만들기",
+  },
+  {
+    mainImg:
+      "https://velog.velcdn.com/images/jinyoung985/post/c9c41a3c-d588-4093-aada-a54bb42945d9/image.gif",
+    isMobile: false,
+    title: "FLOrida",
+    tags: ["전체", "팀 프로젝트"],
+    subTitle: "팀 프로젝트 (6인)",
+    mainTechs: ["JavaScript", "React.js", "styled-components"],
+    introduction: (
+      <>
+        FLO 공식 홈페이지를 모티브로 한 프로젝트입니다.
+        <br />
+        주요 기능으로는 로그인 및 회원가입, 음악 재생, 보관함에 음악 저장,
+        이용권 구매 등이 있습니다.
+      </>
+    ),
+    url: "https://velog.io/@jinyoung985/Portfolio-FLOrida-music",
+  },
+  {
+    mainImg:
+      "https://velog.velcdn.com/images/jinyoung985/post/832d2f12-819f-4282-8da1-f79f1a26ca72/image.gif",
+    isMobile: false,
+    title: "ROECY’ CHOCOLATE",
+    tags: ["전체", "팀 프로젝트"],
+    subTitle: "팀 프로젝트 (6인)",
+    mainTechs: ["JavaScript", "React.js", "sass"],
+    introduction: (
+      <>
+        ROYCE’ Chocolate 공식 홈페이지를 모티브로 한 프로젝트입니다. <br />
+        주요 기능으로는 로그인 및 회원가입, 상품 장바구니에 담기, 구매 등이
+        있습니다.
+      </>
+    ),
+    url: "https://velog.io/@jinyoung985/Portfolio-ROECY-Chocolate",
   },
 ];
