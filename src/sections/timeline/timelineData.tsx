160404c4c27dc61f37fb8256dc77bec7233a0cea
김진영 <jinyoung01099@gmail.com>
2023-02-23 19:15:47 +0900

    [Fix] time;ineData 이미지 교체 및 image url로 변경

--- src/sections/timeline/timelineData.tsx
+++ src/sections/timeline/timelineData.tsx
@@ -3,7 +3,7 @@ import { AiFillCode } from "react-icons/ai";
 import soldier from "../../assets/soldier.png";
 
 export interface timelineDataType {
-  icon: JSX.Element | string;
+  icon: string;
   title: string;
   period: string;
   introduce: string;
@@ -11,25 +11,25 @@ export interface timelineDataType {
 
 export const timelineData: timelineDataType[] = [
   {
-    icon: <GiBriefcase className="icon" />,
+    icon: "https://iconmonstr.com/wp-content/g/gd/makefg.php?i=../releases/preview/2013/png/iconmonstr-briefcase-15.png&r=0&g=0&b=0",
     title: "Pre-Onboarding 코스 수료",
     period: "2022.09 ~ 2022.10",
     introduce: "Wanted에서 진행하는 개발자 인턴십 체험 프로젝트",
   },
   {
-    icon: <AiFillCode className="icon" />,
+    icon: "https://iconmonstr.com/wp-content/g/gd/makefg.php?i=../releases/preview/2013/png/iconmonstr-code-12.png&r=0&g=0&b=0",
     title: "JUSTCODE 부트캠프 6기 수료",
     period: "2022.07 ~ 2022.10",
     introduce: "웹 프론트엔드 개발자 양성 과정",
   },
   {
-    icon: <img src={soldier} alt="soldier" className="icon" />,
+    icon: "https://iconmonstr.com/wp-content/g/gd/makefg.php?i=../releases/preview/2013/png/iconmonstr-radio-9.png&r=0&g=0&b=0",
     title: "제203특공여단 병장 만기전역",
     period: "2020.03 ~ 2021.10",
     introduce: "육군 제203특공여단에서 통신병으로 복무",
   },
   {
-    icon: <GiGraduateCap className="icon" />,
+    icon: "https://iconmonstr.com/wp-content/g/gd/makefg.php?i=../releases/preview/2017/png/iconmonstr-education-1.png&r=0&g=0&b=0",
     title: "유한대학교 기계공학과 졸업",
     period: "2018.03 ~ 2020.02",
     introduce: "",
