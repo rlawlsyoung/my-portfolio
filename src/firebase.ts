1f25f665dacdbfb6fcd2bf25f1797f3f7909e2ed
김진영 <jinyoung01099@gmail.com>
2023-02-24 18:14:00 +0900

    [Add] firebase config 환경변수로 관리

--- src/firebase.ts
+++ src/firebase.ts
@@ -7,13 +7,13 @@ import "firebase/compat/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCWBdFHezQnDZ9Gwjdxv1qhByy0mt0tGY8",
-  authDomain: "portfolio-data-a9faa.firebaseapp.com",
-  projectId: "portfolio-data-a9faa",
-  storageBucket: "portfolio-data-a9faa.appspot.com",
-  messagingSenderId: "283222525396",
-  appId: "1:283222525396:web:d844c029927c827c158241",
-  measurementId: "G-B6ZX0DXWFK",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 firebase.initializeApp(firebaseConfig);
